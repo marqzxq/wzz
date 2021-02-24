3f86f1e836c2a8ea7315f7a83ebcee5d1ae5788c
marqzxq <77423720+marqzxq@users.noreply.github.com>
2021-02-23 23:50:31 -0300

    Add files via upload

---
+++ src/addfoto.js
@@ -0,0 +1,11 @@
+const addfoto = () => { 
+	return `
+	
+	*ADICIONAR BASE DE DADOS FOTO*
+	
+	FOTOS SALVADAS COM SUCESSO NOS DADOS !
+	
+
+obrigado !`
+}
+exports.addfoto = addfoto
---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 IKLAN 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10$ / GRUPO (MÊS)*
+╠➥ *CRIAR: 25$ (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *MERCADO PAGO,PIC PAY*
+╠═══════════════════════════
+╠➥ *CONTATO*
+╠➥ *wa.me/+5511977782073*
+║
+╚═〘 WZZBOT 〙
+`
+}
+exports.iklan = iklan
